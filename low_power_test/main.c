5dede66d559b913d717b14ee31b0aa06a8418385
Hyungsin <hskim@berkeley.edu>
2016-09-28 12:55:11 -0700

    4.1

--- low_power_test/main.c
+++ low_power_test/main.c
@@ -11,19 +11,19 @@
 #include "net/gnrc/udp.h"
 #include "net/gnrc/netapi.h"
 #include "net/gnrc/netreg.h"
+
+#include "arch/lpm_arch.h"
+#include "periph/rtt.h"
 #include <at30ts74.h>
 #include <mma7660.h>
 #include <periph/gpio.h>
 
 // 1 second, defined in us
-#define OFF_INTERVAL (1000000UL)
+#define OFF_INTERVAL (RTT_FREQUENCY)
 #define NETWORK_RTT_US 1000000
 #define ON_INTERVAL (1000000UL)
 void cb(void* arg);
-//void periodic_task(void* arg);
-xtimer_t * timer;
-at30ts74_t tmp;
-mma7660_t acc;
+void periodic_task(void* arg);
 
 void low_power_init(void) {
 
@@ -32,64 +32,56 @@ void low_power_init(void) {
     gpio_write(GPIO_PIN(0, 28), 1); 
     
     // Temperature sensor off
+    at30ts74_t tmp;
     if (at30ts74_init(&tmp, I2C_0, AT30TS74_ADDR, AT30TS74_12BIT) != 0)
         printf("Failed to init TEMP\n");
     
     // Accelerometer off
+    mma7660_t acc;
     if (mma7660_init(&acc, I2C_0, MMA7660_ADDR) != 0)
       printf("Failed to init ACC\n");
     if (mma7660_set_mode(&acc, 0, 0, 0, 0) != 0)
       printf("Failed to set idle mode\n");    
-	if (mma7660_config_samplerate(&acc, MMA7660_SR_AM64, MMA7660_SR_AW32, 1) != 0)
-      printf("Failed to config SR\n");
-    
-
+ 
     printf("Sensors Off\n");
-    //rtt_init();   
-    //printf("RTT initialization (%u %u)\n", RTT_FREQUENCY, RTT_MAX_VALUE);    
+    rtt_init();   
+    printf("RTT initialization (%u %u)\n", RTT_FREQUENCY, RTT_MAX_VALUE);
+    
+    // CPU idle (main clock is active)    
 }
 
 void cb(void* arg) {
-	int32_t temp = 0;
-	int8_t x = 0;
-	int8_t y = 0;
-	int8_t z = 0;
-
-	printf("[%lu] Sensing starts\n",  _xtimer_usec_from_ticks(xtimer_now()));
-
-	at30ts74_read(&tmp, &temp);
-
-    if (mma7660_set_mode(&acc, 1, 0, 0, 0) != 0)
-    	printf("Failed to set active mode\n"); 
-    if (mma7660_read(&acc, &x, &y, &z))
-		printf("Faile to read accel\n");    
-    if (mma7660_set_mode(&acc, 0, 0, 0, 0) != 0)
-    	printf("Failed to set idle mode\n"); 
-
-	printf("[%lu] temperature: %luC / accel %d %d %d\n", _xtimer_usec_from_ticks(xtimer_now()),
-			temp, x, y, z);
-
-	//periodic_task(0);
+	printf("call back\n");
+    //clk_init();
+	periodic_task(0);
 }
 
-/*void periodic_task(void* arg) {
+void periodic_task(void* arg) {
 	uint32_t now;
-
+    uint32_t i =0; //uint32_t last_wakeup = xtimer_now();
+	//printf("0\n");
+    while (i < 1000) {
+		i++;
+	}
+    //xtimer_periodic_wakeup(&last_wakeup, ON_INTERVAL);
+	//lpm_end_awake();
+	//printf("1\n");
 	now = rtt_get_counter() + OFF_INTERVAL;
+	//printf("2\n");
 	now = (now > RTT_MAX_VALUE) ? now - RTT_MAX_VALUE : now;
-	printf("[%lu] sleep\n\n", rtt_get_counter());
+
+	//printf("next wakeup: %lu\n", now);
+    //printf("sleep mode %u\n", lpm_set(LPM_OFF));
     rtt_set_alarm(now, cb, 0);
-}*/
+    //lpm_set(LPM_OFF);   
+}
 
 int main(void)
 {
     low_power_init();
-    //periodic_task(0);
-
-	while (1) {	
-		cb(0);
-		xtimer_usleep(1000000UL);
-	}
-
+    periodic_task(0);
+    while(1) {
+    	xtimer_usleep(1000000U);
+    }
     return 0;
 }
---
+++ low_power_test_3/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <rtt_stdio.h>
+#include "shell.h"
+#include "thread.h"
+#include "xtimer.h"
+#include <string.h>
+
+#include "msg.h"
+#include "net/gnrc.h"
+#include "net/gnrc/ipv6.h"
+#include "net/gnrc/udp.h"
+#include "net/gnrc/netapi.h"
+#include "net/gnrc/netdev2.h"
+#include "net/gnrc/netreg.h"
+#include <at30ts74.h>
+#include <mma7660.h>
+#include <periph/gpio.h>
+
+// 1 second, defined in us
+#define OFF_INTERVAL (1000000UL)
+
+void cb(void* arg);
+
+xtimer_t * timer;
+at30ts74_t tmp;
+mma7660_t acc;
+kernel_pid_t radio;
+netopt_state_t radio_state;
+
+
+void low_power_init(void) {
+
+    // Light sensor off
+    gpio_init(GPIO_PIN(0,28), GPIO_OUT);
+    gpio_write(GPIO_PIN(0, 28), 1);
+
+    // Temperature sensor off
+    if (at30ts74_init(&tmp, I2C_0, AT30TS74_ADDR, AT30TS74_12BIT) != 0)
+        printf("Failed to init TEMP\n");
+
+    // Accelerometer off
+    if (mma7660_init(&acc, I2C_0, MMA7660_ADDR) != 0)
+      printf("Failed to init ACC\n");
+    if (mma7660_set_mode(&acc, 0, 0, 0, 0) != 0)
+      printf("Failed to set idle mode\n");
+	if (mma7660_config_samplerate(&acc, MMA7660_SR_AM64, MMA7660_SR_AW32, 1) != 0)
+      printf("Failed to config SR\n");
+    printf("Sensors Off\n");
+
+	// Radio off
+	radio = gnrc_netdev2_getRadioID(0);
+	radio_state = NETOPT_STATE_SLEEP;
+	gnrc_netapi_set(radio, NETOPT_STATE, 0, &radio_state, sizeof(netopt_state_t));
+    printf("Radio off\n");
+		
+}
+
+
+void cb(void* arg) {
+
+	int32_t temp = 0;
+	int8_t x = 0;
+	int8_t y = 0;
+	int8_t z = 0;
+
+	printf("[%lu] Sensing starts\n",  xtimer_usec_from_ticks(xtimer_now()));
+
+	at30ts74_read(&tmp, &temp);
+
+    if (mma7660_set_mode(&acc, 1, 0, 0, 0) != 0)
+    	printf("Failed to set active mode\n");
+    if (mma7660_read(&acc, &x, &y, &z))
+		printf("Faile to read accel\n");
+    if (mma7660_set_mode(&acc, 0, 0, 0, 0) != 0)
+    	printf("Failed to set idle mode\n");
+
+	printf("[%lu] temperature: %luC / accel %d %d %d\n", xtimer_usec_from_ticks(xtimer_now()),
+			temp, x, y, z);
+
+	radio_state = NETOPT_STATE_IDLE;
+	gnrc_netapi_set(radio, NETOPT_STATE, 0, &radio_state, sizeof(netopt_state_t));
+	radio_state = NETOPT_STATE_SLEEP;
+	gnrc_netapi_set(radio, NETOPT_STATE, 0, &radio_state, sizeof(netopt_state_t));
+
+}
+
+int main(void)
+{
+    low_power_init();
+
+	while (1) {
+		cb(0);
+		xtimer_usleep(OFF_INTERVAL);
+	}
+
+    return 0;
+}
